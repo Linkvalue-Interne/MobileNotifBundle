22396cbe173fab40a0fedf690cf29f843bef6f42
Jamal Youssefi <jamal.youssefi@gmail.com>
2015-09-25 16:36:04 +0200

    update config extension

--- DependencyInjection/LinkValueMobileNotifExtension.php
+++ DependencyInjection/LinkValueMobileNotifExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Definition;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -22,7 +23,19 @@ class LinkValueMobileNotifExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        var_dump($config); exit;
+        $clientNamespace = "LinkValue\MobileNotifBundle\Bridge\Mobile\Android";
+
+        foreach ($config["clients"] as $type => $clients) {
+
+            foreach ($clients as $name => $params) {
+
+                $clientClass = $type == "ios" ? "AppleMobileClient" : "AndroidMobileClient";
+
+                $client = new Definition($clientNamespace . "\\" . $clientClass, $params);
+
+                $container->setDefinition($name, $client);
+            }
+        }
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
