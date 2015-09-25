1f306d0639889408bdd4e35427a00246ed48a82f
Valentin Coulon <valentin.c0610@gmail.com>
2015-09-25 16:17:34 +0200

    Create notif command

--- Command/NotifCommand.php
+++ Command/NotifCommand.php
@@ -1,24 +1,45 @@
 <?php
 
-use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use LinkValue\MobileNotifBundle\Entity\MobileClient\MobileMessage;
 
-class NotifCommand extends Command
+class NotifCommand extends ContainerAwareCommand
 {
     /**
-     * {@inheritdoc}
+     * Command configuration.
      */
     protected function configure()
     {
-
+        $this
+            ->setName('mobilenotif:push')
+            ->setDescription('Push notification command.')
+            ->addArgument('device_token', InputArgument::REQUIRED, 'Token of the device which will receive the notification.')
+            ->addArgument('message', InputArgument::REQUIRED, 'Notification message.')
+            ->addOption('mobile-client', 'm', InputOption::VALUE_REQUIRED, 'Mobile client to use (use the first one by default)', null)
+        ;
     }
 
     /**
-     * {@inheritdoc}
+     * Command execution.
+     *
+     * @codeCoverageIgnore
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        /* @var $mobileNotif \LinkValue\MobileNotifBundle\MobileNotif */
+        $mobileNotif = $this->getContainer()->get('link_value_mobile_notif.mobile_notif');
 
+        $mobileClientKey = $input->getOption('mobile-client');
+        if (!is_null($mobileClientKey)) {
+            $mobileNotif->using($mobileClientKey);
+        }
+
+        $message = (new MobileMessage())
+            ->setDeviceToken($input->getArgument('device_token'))
+            ->setMessage($input->getArgument('message'))
+        ;
+        $mobileNotif->push($message);
     }
 }
