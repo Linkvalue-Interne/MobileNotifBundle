269d5ed54a6fc83338828b64cbcabc2ef025d90e
Jamal Youssefi <jamal.youssefi@gmail.com>
2015-10-23 12:42:56 +0200

    update config and extension

--- DependencyInjection/LinkValueMobileNotifExtension.php
+++ DependencyInjection/LinkValueMobileNotifExtension.php
@@ -7,6 +7,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -27,11 +28,19 @@ class LinkValueMobileNotifExtension extends Extension
 
         foreach ($config["clients"] as $type => $clients) {
 
-            foreach ($clients as $name => $params) {
+            foreach ($clients as $name => $data) {
+                $services = isset($data['services']) ? $data['services'] : array();
+                $params = isset($data['params']) ? $data['params'] : array();
 
                 $clientClass = $type == "ios" ? "AppleMobileClient" : "AndroidMobileClient";
 
-                $client = new Definition($clientNamespace . "\\" . $clientClass, $params);
+                $client = new Definition($clientNamespace . "\\" . $clientClass);
+
+                foreach ($services as $service_id) {
+                    $client->addArgument(new Reference($service_id));
+                }
+
+                $client->addMethodCall('setUp', $params);
 
                 $container->setDefinition($name, $client);
             }
