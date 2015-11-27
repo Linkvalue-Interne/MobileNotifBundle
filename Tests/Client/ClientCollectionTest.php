b20979fdc8a66e903d2c8dec54a2448d65b6f948
Raphael De Freitas <raphael@de-freitas.net>
2015-11-27 12:50:37 +0100

    Testing class ClientCollection

---
+++ Tests/Client/ClientCollectionTest.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of the MobileNotifBundle package.
+ *
+ * (c) Raphael De Freitas <raphael@de-freitas.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace LinkValue\MobileNotifBundle\Tests\Client\ClientCollection;
+
+use LinkValue\MobileNotif\Client\ClientInterface;
+use LinkValue\MobileNotif\Model\Message;
+use LinkValue\MobileNotifBundle\Client\ClientCollection;
+
+/**
+ * Class ClientCollectionTest
+ *
+ * @author Raphael De Freitas <raphael@de-freitas.net>
+ */
+class ClientCollectionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAddClient()
+    {
+        $clientCollection = new ClientCollection();
+        $testClient = new TestClient();
+        $clientCollection->addClient('test', $testClient);
+        $this->assertEquals($testClient, $clientCollection->get('test'));
+    }
+
+    public function testAddClientWithNonStringKey()
+    {
+        $clientCollection = new ClientCollection();
+        $this->setExpectedException('\InvalidArgumentException');
+        $clientCollection->addClient(42, new TestClient());
+    }
+
+    public function testAddClientTwice()
+    {
+        $clientCollection = new ClientCollection();
+        $testClient = new TestClient();
+        $clientCollection->addClient('test', $testClient);
+        $this->setExpectedException('\RuntimeException');
+        $clientCollection->addClient('test', $testClient);
+    }
+}
+
+class TestClient implements ClientInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function setUp(array $params)
+    {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function push(Message $message)
+    {
+    }
+
+}
