b548b278f6f1e209b9f12dde3187ea1c8365876b
Raphael De Freitas <raphael@de-freitas.net>
2015-11-28 09:05:01 +0100

    [Work In Progress] Working on tets

---
+++ Tests/Client/GcmClientTest.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the MobileNotifBundle package.
+ *
+ * (c) Raphael De Freitas <raphael@de-freitas.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace LinkValue\MobileNotifBundle\Tests\Client\ClientCollection;
+
+/**
+ * Class GcmClientTest
+ *
+ * @author Raphael De Freitas <raphael@de-freitas.net>
+ */
+class GcmClientTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDefault()
+    {
+    }
+}
---
+++ Tests/Command/NotifCommandTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the MobileNotifBundle package.
+ *
+ * (c) Raphael De Freitas <raphael@de-freitas.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace LinkValue\MobileNotifBundle\Tests\Command\NotifCommand;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Class NotifCommandTest
+ *
+ * @author Raphael De Freitas <raphael@de-freitas.net>
+ */
+class NotifCommandTest extends WebTestCase
+{
+    public function testDefault()
+    {
+    }
+}
