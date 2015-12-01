223729ed2f03cd38dbf5e3cfa3bcf2abffddeb42
Raphael De Freitas <raphael@de-freitas.net>
2015-12-01 15:06:32 +0100

    [WIP] AppleClientTest

--- Tests/Client/AppleClientTest.php
+++ Tests/Client/AppleClientTest.php
@@ -10,6 +10,11 @@
  */
 
 namespace LinkValue\MobileNotifBundle\Tests\Client\ClientCollection;
+use LinkValue\MobileNotif\Model\Message;
+use LinkValue\MobileNotifBundle\Client\AppleClient;
+use LinkValue\MobileNotifBundle\Profiler\NotifProfiler;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Stopwatch\StopwatchEvent;
 
 /**
  * Class AppleClientTest
@@ -18,7 +23,75 @@ namespace LinkValue\MobileNotifBundle\Tests\Client\ClientCollection;
  */
 class AppleClientTest extends \PHPUnit_Framework_TestCase
 {
-    public function testDefault()
+    protected $client;
+
+    /**
+     * @return LoggerInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getDummyLogger()
     {
+        $dummyLogger = $this->getMock('Psr\Log\LoggerInterface');
+        return $dummyLogger;
+    }
+
+    /**
+     * @return NotifProfiler|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getDummyNotifProfiler()
+    {
+        $dummyStopwatch = $this->getMock('Symfony\Component\Stopwatch\Stopwatch');
+        $dummyNotifProfiler = $this->getMock('LinkValue\MobileNotifBundle\Profiler\NotifProfiler', array(), array($dummyStopwatch));
+        return $dummyNotifProfiler;
+    }
+
+    /**
+     * @return Message|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getDummyMessage()
+    {
+        $dummyMessage = $this->getMock('LinkValue\MobileNotif\Model\Message');
+        return $dummyMessage;
+    }
+
+    /**
+     * @return StopwatchEvent|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getDummyStopwatchEvent()
+    {
+        $dummyStopwatchEvent = $this->getMock('Symfony\Component\Stopwatch\StopwatchEvent', array(), array('12345', 'category'));
+        return $dummyStopwatchEvent;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp()
+    {
+        $this->client = new AppleClient($this->getDummyLogger(), $this->getDummyNotifProfiler());
+    }
+
+    public function testProfilerStartAndStop()
+    {
+        $dummyStopwatchEvent = $this->getDummyStopwatchEvent();
+        $dummyNotifProfiler = $this->getDummyNotifProfiler();
+        $dummyNotifProfiler
+            ->expects($this->once())
+            ->method('startProfiling')
+            ->will($this->returnValue($dummyStopwatchEvent));
+        $dummyNotifProfiler
+            ->expects($this->once())
+            ->method('stopProfiling')
+            ->will($this->returnValue(null));
+
+        $client = new AppleClient($this->getDummyLogger(), $dummyNotifProfiler);
+
+        $dummyMessage = $this->getDummyMessage();
+        $dummyMessage
+            ->expects($this->any())
+            ->method('getContent')
+            ->will($this->returnValue('MyMessage'));
+
+        $client->push($dummyMessage);
+
     }
 }
